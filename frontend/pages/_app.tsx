0c3c67a031a370d1996be2b48ec2cb8734b718ed
Roland <gavrilescu.roland@gmail.com>
2022-06-25 10:23:57 -0400

    moralis sdk and linting

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl="https://lznfuhex1lqb.usemoralis.com:2053/server"
+      appId="LRic9DDI2FTI3eOPxHbZMvfAz0KBXMAPZeGyumTr"
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
